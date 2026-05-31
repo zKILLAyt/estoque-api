aac879efe5eb99d4ceff0b0db697877a842507d1
Gustavo Gomes <ferreiragu84@gmail.com>
2026-05-31 20:56:21 -0300

    Primeira versão da API de estoque

---
+++ src/database/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao MySQL:", err);
+    return;
+  }
+
+  console.log("MySQL conectado!");
+});
+
+module.exports = connection;
