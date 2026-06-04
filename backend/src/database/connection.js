5617116fc4b86bc6befee651c0bc9c019dc941c8
Gustavo Gomes <ferreiragu84@gmail.com>
2026-06-04 13:11:07 -0300

    Adiciona debug

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
